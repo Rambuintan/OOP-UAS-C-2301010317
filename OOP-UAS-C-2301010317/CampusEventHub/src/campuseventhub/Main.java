b4f00f6ac1e326c958df7a5f42806a2099f2084a
Rambuintan <rambuintan08@gmail.com>
2025-06-23 17:22:14 +0800

    Delete OOP-UAS-C-2301010317/CampusEventHub/src/campuseventhub directory

--- OOP-UAS-C-2301010317/CampusEventHub/src/campuseventhub/Main.java
+++
@@ -1,8 +0,0 @@
-package campuseventhub;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Selamat datang di CampusEventHub!");
-        // Di sini akan dipanggil menu utama / tampilan awal
-    }
-}
